49eaea7b47917c580a13ea568e5f8d41d45c8596
FalconSN <murathancw@gmail.com>
2024-04-07 03:39:29 +0300

    cook a cookie

---
+++ extract_cookie.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+# Chromium Cookies file
+cookies_file="$HOME/Cookies";
+host_key="$1";
+
+read -r name value domain path httponly expiration < <(
+sqlite3 "$cookie_file" << EOF
+.mode tabs
+select name,value,host_key,path,is_httponly,expires_utc
+from cookies
+where host_key = "$host_key" and name = 'session'
+EOF
+)
+
+# Convert win32 epoch to unix
+expiration=$((($expiration/1000000)-11644473600));
+
+(( httponly == 1 )) && {
+    httponly=TRUE;
+} || {
+    httponly=FALSE;
+}
+
+[ ${domain:0:1} = '.' ] || {
+    domain=".${domain}"
+}
+
+netscape_fmt="# Netscape HTTP Cookie File
+"
+
+for var in "$domain" "TRUE" "$path" "$httponly" \
+    "$expiration" "$name" "$value"; do
+    netscape_fmt+="$var"$'\t';
+done
+
+netscape_fmt+=$'\n';
+echo "$netscape_fmt" > "${host_key}-cookie.txt"
+
