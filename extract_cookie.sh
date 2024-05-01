fdf271a75595c8b7efb80d9ec41b5ee47bc505cc
FalconSN <murathancw@gmail.com>
2024-05-01 13:04:09 +0300

    bugfix

--- extract_cookie.sh
+++ extract_cookie.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
-set -e
-# Chromium Cookies file
+
+# chromium cookies file
 cookies_file="$HOME/Cookies";
 host_key="$1";
-
-read -r name value domain path httponly expiration < <(
-sqlite3 "$cookies_file" << EOF
-.mode tabs
-select name,value,host_key,path,is_httponly,expires_utc
-from cookies
-where host_key = "$host_key" and name = 'session'
-EOF
-)
-
-# Convert win32 epoch to unix
-expiration=$((($expiration/1000000)-11644473600));
-
-(( httponly == 1 )) && {
-    httponly=TRUE;
-} || {
-    httponly=FALSE;
-}
-
-[ ${domain:0:1} = '.' ] || {
-    domain=".${domain}"
-}
 
 netscape_fmt="# Netscape HTTP Cookie File
 "
 
-for var in "$domain" "TRUE" "$path" "$httponly" \
-    "$expiration" "$name" "$value"; do
-    netscape_fmt+="$var"$'\t';
-done
+case "$host_key" in
+*[%_]*)
+    where="like '$host_key'";;
+*)
+    where="= '$host_key'";;
+esac
 
-netscape_fmt+=$'\n';
-echo "$netscape_fmt" > "${host_key}-cookie.txt"
+t=$'\t';
+n=$'\n';
+while read -r name value domain path httponly expiration; do
+    # convert win32 epoch to unix
+    (( expiration > 0 )) && {
+        expiration=$(((expiration / 1000000 ) - 11644473600 ));
+    }
+
+    (( httponly == 1 )) && {
+        httponly=TRUE;
+    } || {
+        httponly=FALSE;
+    }
+
+    netscape_fmt+="$domain${t}TRUE$t$path$t$httponly$t$expiration$t$name$t$value$n";
+done < <(
+sqlite3 "$cookies_file" << EOF
+.mode tabs
+select name,value,host_key,path,is_httponly,expires_utc
+from cookies
+where host_key $where
+EOF
+)
+
+echo "$netscape_fmt" > "${host_key//%/}-cookie.txt"
 
